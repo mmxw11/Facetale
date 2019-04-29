296395a5e2fe55f5c73b24eeac4d59abdbe57858
mmxw11 <mmxw11@users.noreply.github.com>
2019-04-29 23:48:25 +0300

    Fix height calculations + share some css elements

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -1,7 +1,17 @@
-function resizeCommentBoxTextarea(element) {
-    element.style.height = "auto";
-    element.style.height = element.scrollHeight + 3 + "px";
+function resizeCommentBoxTextarea(e) {
+    e.style.height = "auto";
+
+    var cstyle = getComputedStyle(e);
+    var crect = e.getBoundingClientRect();
+    var paddingY = parseFloat(cstyle.paddingTop) + parseFloat(cstyle.paddingBottom);
+    var newHeight = e.scrollHeight - paddingY;
+    // crect.height is used rather than clientHeight to ensure the height is always more on the first line.
+    // This is because scrollHeight is an integer and some browsers round it up and others down.
+    if (e.scrollHeight > crect.height) {
+        e.style.height = newHeight + "px";
+    }
 }
+
 window.addEventListener("resize", function (e) {
     var commentboxes = document.getElementsByClassName("commentbox-textarea");
     for (let i = 0; i < commentboxes.length; i++) {
