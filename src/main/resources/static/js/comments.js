58773865a1ae026a207d36edf606b8d34f1e9f62
mmxw11 <mmxw11@users.noreply.github.com>
2019-05-01 17:51:35 +0300

    Add cancel comment button and css for form errors

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -13,9 +13,23 @@ function resizeCommentBoxTextarea(e) {
 }
 
 window.addEventListener("resize", function (e) {
-    var commentboxes = document.getElementsByClassName("commentbox-textarea");
+    var commentboxes = document.getElementsByClassName("comment-textarea");
     for (let i = 0; i < commentboxes.length; i++) {
         const commentbox = commentboxes[i];
         resizeCommentBoxTextarea(commentbox);
+    }
+});
+
+window.addEventListener("click", function (e) {
+    if (!Element.prototype.matches) {
+        // IE fix.
+        Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+    }
+    var element = e.target;
+    if (!element.matches(".comment-textarea")) {
+        return;
+    }
+    if (element.classList.contains("fe-invalid-value")) {
+        element.classList.remove("fe-invalid-value");
     }
 });
